98040e98d4bc1a304d39b2a744fbf5c29ed66179
Alexandre Alapetite <alexandre@alapetite.fr>
2015-05-03 14:44:05 +0200

    Utils.java: removed unused code

--- src/com/pursuer/reader/easyrss/Utils.java
+++ src/com/pursuer/reader/easyrss/Utils.java
@@ -15,24 +15,13 @@ import java.lang.reflect.Array;
 import java.net.URLDecoder;
 import java.net.URLEncoder;
 import java.util.Date;
-import org.apache.http.HttpVersion;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.impl.conn.tsccm.ThreadSafeClientConnManager;
-import org.apache.http.params.BasicHttpParams;
-import org.apache.http.params.HttpParams;
-import org.apache.http.params.HttpProtocolParams;
-import org.apache.http.protocol.HTTP;
-import org.apache.http.conn.scheme.SchemeRegistry;
-import org.apache.http.conn.scheme.PlainSocketFactory;
-import org.apache.http.conn.ssl.SSLSocketFactory;
-import org.apache.http.conn.scheme.Scheme;
+
+import android.content.Context;
+import android.database.Cursor;
 
 import com.pursuer.reader.easyrss.account.ReaderAccountMgr;
 import com.pursuer.reader.easyrss.data.DataMgr;
 import com.pursuer.reader.easyrss.network.NetworkMgr;
-
-import android.content.Context;
-import android.database.Cursor;
 
 final public class Utils {
     @SuppressWarnings("unchecked")
@@ -49,23 +38,6 @@ final public class Utils {
             start += array.length;
         }
         return mergedArray;
-    }
-
-    public static DefaultHttpClient createHttpClient() {
-        final HttpParams config = new BasicHttpParams();
-        HttpProtocolParams.setVersion(config, HttpVersion.HTTP_1_1);
-        HttpProtocolParams.setContentCharset(config, HTTP.UTF_8);
-        HttpProtocolParams.setUserAgent(config, Utils.class.getName());
-
-        final SchemeRegistry reg = new SchemeRegistry();
-        reg.register(new Scheme("http", PlainSocketFactory.getSocketFactory(), 80));
-        reg.register(new Scheme("https", SSLSocketFactory.getSocketFactory(), 443));
-
-        final ThreadSafeClientConnManager manager = new ThreadSafeClientConnManager(config, reg);
-
-        final DefaultHttpClient client = new DefaultHttpClient(manager, config);
-        client.getParams().setParameter("http.socket.timeout", 30 * 1000);
-        return client;
     }
 
     @SuppressWarnings("deprecation")
