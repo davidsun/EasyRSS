707227d91fc1858238c70a4cde743512c92000b2
Alexandre Alapetite <alexandre@alapetite.fr>
2015-05-24 21:53:18 +0200

    Credits for HorizontalPager
    https://github.com/davidsun/horizontalpager
    https://github.com/ysamlan/horizontalpager
    Apache 2 can be included in GNU GPL project

--- src/org/freshrss/easyrss/view/HorizontalPager.java
+++ src/org/freshrss/easyrss/view/HorizontalPager.java
@@ -1,14 +1,25 @@
-/*******************************************************************************
- * Copyright (c) 2012 Pursuer (http://pursuer.me).
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the GNU Public License v3.0
- * which accompanies this distribution, and is available at
- * http://www.gnu.org/licenses/gpl.html
- * 
- * Contributors:
- *     Pursuer - initial API and implementation
- ******************************************************************************/
-
+/**
+ * Modified by Sun Zheng https://github.com/davidsun/horizontalpager
+ * based on modifications by Yoni Samlan https://github.com/ysamlan/horizontalpager
+ * based on RealViewSwitcher, whose license is:
+ *
+ * Copyright (C) 2010 Marc Reichelt
+ *
+ * Work derived from Workspace.java of the Launcher application
+ *  see http://android.git.kernel.org/?p=platform/packages/apps/Launcher.git
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.freshrss.easyrss.view;
 
 import android.content.Context;
